697056c18a1eeafc0680684e03347f0c08c58b43
SkimoBen <pearmanbenjamin@gmail.com>
2023-06-09 11:36:43 -0600

    Initial Commit

---
+++ Calico AI/Calico_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calico_AIApp.swift
+//  Calico AI
+//
+//  Created by Ben Pearman on 2023-06-09.
+//
+
+import SwiftUI
+
+@main
+struct Calico_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
